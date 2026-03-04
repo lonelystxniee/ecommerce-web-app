e98627a0e8087c981049e3c55112adec9b098f98
Nguyen Long <longthoi0501@gmail.com>
2026-03-04 22:26:01 +0700

    Resolve merge: keep ours backend/package-lock.json and fix frontend App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
-import { CartProvider } from "./context/CartContext";
 import CategoryPage from "./pages/CategoryPage";
-import ScrollToTop from "./components/ScrollToTop";
 import AccountPage from "./pages/AccountPage";
 import OrderTracking from "./pages/OrderTracking";
 import AdminOrders from "./pages/AdminOrders";
+import ResetPassword from "./pages/ResetPassword";
+
+
+import { CartProvider } from "./context/CartContext";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
@@ -26,16 +31,16 @@ function App() {
               <Route path="/cart" element={<Cart />} />
               <Route path="/checkout" element={<Checkout />} />
               <Route path="/category/:slug" element={<CategoryPage />} />
-
               <Route path="/account" element={<AccountPage />} />
+              <Route path="/profile" element={<AccountPage />} />
               <Route path="/order/:id" element={<OrderTracking />} />
               <Route path="/admin/orders" element={<AdminOrders />} />
-
-              <Route path="/profile" element={<AccountPage />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
             </Routes>
           </main>
           <Footer />
           <ScrollToTop />
+          <Toaster position="top-right" />
         </div>
       </Router>
     </CartProvider>
